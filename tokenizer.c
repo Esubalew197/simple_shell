94e091a234c8c8befbd6f90ef94cd82613b6c463
Esubalew197 <Esubalewmillion@gmail.com>
2022-11-12 17:21:07 +0300

    Simple Shell

--- tokenizer.c
+++ tokenizer.c
@@ -2,7 +2,7 @@
 /**
  * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
- * @d: the delimeter string
+ * @d: the delimeter string.
  * Return: a pointer to an array of strings, or NULL on failure.
  */
 char **strtow(char *str, char *d)
@@ -49,7 +49,7 @@ char **strtow(char *str, char *d)
  * **strtow2 - splits a string into words
  * @str: the  input string
  * @d: the delimeter
- * Return: a pointer to an array of strings, or NULL on failure
+ * Return: a pointer to an array of strings, or NULL on failure.
  */
 char **strtow2(char *str, char d)
 {
