e8f41e49209f1e44e3d4575513c071ee53a914a0
Esubalew197 <Esubalewmillion@gmail.com>
2022-11-12 17:23:28 +0300

    Simple Shell

--- history.c
+++ history.c
@@ -25,7 +25,7 @@ char *get_history_file(info_t *info)
  * write_history - creates a file, or appends to an existing file
  * @info: the parameter struct
  *
- * Return: 1 on success, else -1
+ * Return: 1 on success, else -1.
  */
 int write_history(info_t *info)
 {
@@ -53,9 +53,8 @@ int write_history(info_t *info)
  * read_history - reads history from file
  * @info: the parameter struct
  *
- * Return: histcount on success, 0 otherwise
+ * Return: histcount on success, 0 otherwise.
  */
-
 int read_history(info_t *info)
 {
 	int i, last = 0, linecount = 0;
